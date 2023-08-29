62e9cb26ee3df0968aa829d9965bfd078389afa0
Taj Mohammad Anim <143492116+Anim36@users.noreply.github.com>
2023-08-29 15:55:52 +0600

    Add files via upload

---
+++ recursive1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+// Recursive function to print numbers from start to end
+void printNumbers(int start, int end) {
+    if (start < end)
+    {
+        return; // Base case: stop recursion when start > end
+    }
+
+    printf("%d ", start); // Print the current number
+    printNumbers(start - 1, end); // Recursive call for the next number
+}
+int main() {
+    int start = 10;
+    int end = 1;
+
+    printf("Numbers from %d to %d:\n",end,start);
+    printNumbers(start,end);
+
+    return 0;
+}
